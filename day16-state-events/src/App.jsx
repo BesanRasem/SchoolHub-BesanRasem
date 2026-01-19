bfa819b8d4fa9f9075be421de8a2080dd6e3b9a1
BesanRasem <2146310@std.hu.edu.jo>
2026-01-19 19:52:59 +0300

    Task from the lecture on 19/1: Fetching data in React using useEffect

--- day16-state-events/src/App.jsx
+++ day16-state-events/src/App.jsx
@@ -1,82 +1,18 @@
 import './App.css';
-
- 
-
- 
-
-/*
-
-Day 16: State and Events
-
-Goals: Practice useState + events + controlled inputs + mini interactive app (Task Manager).
-
-Hour 1 - 2 - 3
-
- */
-
- 
-
-import Counter from './components/Counter';
-
-import LikeButton from './components/LikeButton';
-
-
-
- 
+import { useEffect } from "react";
 
 function App() {
+     useEffect(() => {
+  fetch("https://dummyjson.com/products")
+    .then(res => res.json())
+    .then(data => console.log(data));
+     }, []);
 
     return (
+        <h1>useEffect</h1>
 
-    <div className="page">
 
-        <header className="topbar">
-
-            <h1>Day 16: State and Events</h1>
-
-            <p>
-
-                useState + events + controlled inputs + mini interactive app (Task Manager).
-
-            </p>
-
-        </header>
-
- 
-
-        <section className="section">
-
-            <h2>Understanding state</h2>
-
-            <p className="muted">
-
-                State belong to a component. When state changes, React re-renders UI.
-
-            </p>
-
-            <div className="grid">
-
-                <Counter />
-
-                <LikeButton />
-
-            </div>
-
-        </section>
-
- 
-
-        <footer className="footer">
-
-            <p className="muted">
-
-                Best practice: never state directly. Always use setState.
-
-            </p>
-
-        </footer>
-
-    </div>
+  
 
     );
 
